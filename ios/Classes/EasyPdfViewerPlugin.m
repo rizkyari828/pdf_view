601cbd0b4b0ba6a4fc1bf097aee57710d27ed8e8
Rizky Ari Pratama <BSI50164@bsi.co.id>
2023-10-19 15:29:46 +0700

    handle crash

--- ios/Classes/EasyPdfViewerPlugin.m
+++ ios/Classes/EasyPdfViewerPlugin.m
@@ -94,59 +94,68 @@ static NSString* kFileName = @"";
 
 -(NSString*)getPage:(NSString *)url ofPage:(size_t)pageNumber
 {
+    
     NSURL * sourcePDFUrl;
-    if([url containsString:kFilePath]){
-        sourcePDFUrl = [NSURL URLWithString:url];
-    }else{
-        sourcePDFUrl = [NSURL URLWithString:[kFilePath stringByAppendingString:url]];
+    @try {
+        if([url containsString:kFilePath]){
+            sourcePDFUrl = [NSURL URLWithString:url];
+        }else{
+            sourcePDFUrl = [NSURL URLWithString:[kFilePath stringByAppendingString:url]];
+        }
     }
-    CGPDFDocumentRef SourcePDFDocument = CGPDFDocumentCreateWithURL((__bridge CFURLRef)sourcePDFUrl);
-    size_t numberOfPages = CGPDFDocumentGetNumberOfPages(SourcePDFDocument);
-    NSArray *paths = NSSearchPathForDirectoriesInDomains(NSCachesDirectory, NSUserDomainMask, YES);
-    NSString *temporaryDirectory = [paths objectAtIndex:0];
-    NSString *filePathAndDirectory = [temporaryDirectory stringByAppendingPathComponent:kDirectory];
-    NSError *error;
-
-    if (pageNumber > numberOfPages) {
-        pageNumber = numberOfPages;
-    }
-
-    if (![[NSFileManager defaultManager] createDirectoryAtPath:filePathAndDirectory
-                                   withIntermediateDirectories:YES
-                                                    attributes:nil
-                                                         error:&error])
-    {
-        NSLog(@"Create directory error: %@", error);
+    @catch (NSException *exception) {
+        NSLog(@"%@", exception.reason);
         return nil;
     }
-    CGPDFPageRef SourcePDFPage = CGPDFDocumentGetPage(SourcePDFDocument, pageNumber);
-    CGPDFPageRetain(SourcePDFPage);
-    NSString *relativeOutputFilePath = [NSString stringWithFormat:@"%@/%@-%d.png", kDirectory, kFileName, (int)pageNumber];
-    NSString *imageFilePath = [temporaryDirectory stringByAppendingPathComponent:relativeOutputFilePath];
-    CGRect sourceRect = CGPDFPageGetBoxRect(SourcePDFPage, kCGPDFMediaBox);
-    UIGraphicsBeginPDFContextToFile(imageFilePath, sourceRect, nil);
-    // Calculate resolution
-    // Set DPI to 300
-    CGFloat dpi = 300.0 / 72.0;
-    CGFloat width = sourceRect.size.width * dpi;
-    CGFloat height = sourceRect.size.height * dpi;
-    UIGraphicsBeginImageContext(CGSizeMake(width, height));
-    // Fill Background
-    CGContextRef currentContext = UIGraphicsGetCurrentContext();
-    // Change interpolation settings
-    CGContextSetInterpolationQuality(currentContext, kCGInterpolationHigh);
-    // Fill background with white color
-    CGContextSetRGBFillColor(currentContext, 1.0f, 1.0f, 1.0f, 1.0f);
-    CGContextFillRect(currentContext, CGContextGetClipBoundingBox(currentContext));
-    CGContextTranslateCTM(currentContext, 0.0, height);
-    CGContextScaleCTM(currentContext, dpi, -dpi);
-    CGContextSaveGState(currentContext);
-    CGContextDrawPDFPage (currentContext, SourcePDFPage);
-    CGContextRestoreGState(currentContext);
-    UIImage *image = UIGraphicsGetImageFromCurrentImageContext();
-    UIGraphicsEndImageContext();
-    [UIImagePNGRepresentation(image) writeToFile: imageFilePath atomically:YES];
-    return imageFilePath;
+    @finally {
+        CGPDFDocumentRef SourcePDFDocument = CGPDFDocumentCreateWithURL((__bridge CFURLRef)sourcePDFUrl);
+        size_t numberOfPages = CGPDFDocumentGetNumberOfPages(SourcePDFDocument);
+        NSArray *paths = NSSearchPathForDirectoriesInDomains(NSCachesDirectory, NSUserDomainMask, YES);
+        NSString *temporaryDirectory = [paths objectAtIndex:0];
+        NSString *filePathAndDirectory = [temporaryDirectory stringByAppendingPathComponent:kDirectory];
+        NSError *error;
+
+        if (pageNumber > numberOfPages) {
+            pageNumber = numberOfPages;
+        }
+
+        if (![[NSFileManager defaultManager] createDirectoryAtPath:filePathAndDirectory
+                                    withIntermediateDirectories:YES
+                                                        attributes:nil
+                                                            error:&error])
+        {
+            NSLog(@"Create directory error: %@", error);
+            return nil;
+        }
+        CGPDFPageRef SourcePDFPage = CGPDFDocumentGetPage(SourcePDFDocument, pageNumber);
+        CGPDFPageRetain(SourcePDFPage);
+        NSString *relativeOutputFilePath = [NSString stringWithFormat:@"%@/%@-%d.png", kDirectory, kFileName, (int)pageNumber];
+        NSString *imageFilePath = [temporaryDirectory stringByAppendingPathComponent:relativeOutputFilePath];
+        CGRect sourceRect = CGPDFPageGetBoxRect(SourcePDFPage, kCGPDFMediaBox);
+        UIGraphicsBeginPDFContextToFile(imageFilePath, sourceRect, nil);
+        // Calculate resolution
+        // Set DPI to 300
+        CGFloat dpi = 300.0 / 72.0;
+        CGFloat width = sourceRect.size.width * dpi;
+        CGFloat height = sourceRect.size.height * dpi;
+        UIGraphicsBeginImageContext(CGSizeMake(width, height));
+        // Fill Background
+        CGContextRef currentContext = UIGraphicsGetCurrentContext();
+        // Change interpolation settings
+        CGContextSetInterpolationQuality(currentContext, kCGInterpolationHigh);
+        // Fill background with white color
+        CGContextSetRGBFillColor(currentContext, 1.0f, 1.0f, 1.0f, 1.0f);
+        CGContextFillRect(currentContext, CGContextGetClipBoundingBox(currentContext));
+        CGContextTranslateCTM(currentContext, 0.0, height);
+        CGContextScaleCTM(currentContext, dpi, -dpi);
+        CGContextSaveGState(currentContext);
+        CGContextDrawPDFPage (currentContext, SourcePDFPage);
+        CGContextRestoreGState(currentContext);
+        UIImage *image = UIGraphicsGetImageFromCurrentImageContext();
+        UIGraphicsEndImageContext();
+        [UIImagePNGRepresentation(image) writeToFile: imageFilePath atomically:YES];
+        return imageFilePath;
+    }
 }
 
 @end
